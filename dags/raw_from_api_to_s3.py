f20772b6b9f805b3fbe7eb1fa0931745b35d6ecb
MrDan1el <mrdan1el@duck.com>
2025-09-23 19:04:53 +0300

    Обновлен DAG raw_from_api_to_s3

--- dags/raw_from_api_to_s3.py
+++ dags/raw_from_api_to_s3.py
@@ -7,13 +7,11 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
-COUNTRY = 'Russian Federation'
-
-
-def get_data_from_api():
+def get_data_from_api(country):
     
     url = 'https://ws.audioscrobbler.com/2.0/'
     headers = {'user-agent': 'username'}
@@ -21,30 +19,38 @@ def get_data_from_api():
         'api_key': Variable.get("api_key"),
         'format': 'json',
         'method': 'geo.getTopTracks',
-        'country': COUNTRY,
+        'country': country,
         'limit': 100
     }
     
-    response = requests.get(url, headers=headers, params=payload)
-    return response.json()
+    try:
+        logging.info(f"Попытка получения данных по API реквесту.")
+        response = requests.get(url, headers=headers, params=payload)
+        return response.json()
+    except Exception as er:
+        logging.error(f'Ошибка при попытке получения данных по API реквесту. {er}')
 
 
 def extract_data_to_s3():
     
-    data = get_data_from_api()
+    country = 'Russian Federation'
+    data = get_data_from_api(country)
+    logging.info(f"Данные из API получены успешно.")
     
-    # Генерируем имя файла
     timestamp = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
-    filename = f"lastfm_api/{COUNTRY}/{COUNTRY}_{timestamp}.json"
-   
-    # Используем S3Hook
-    hook = S3Hook(aws_conn_id='aws_conn')
-    hook.load_string(
-        string_data=json.dumps(data, indent=4),
-        key=filename,
-        bucket_name='bucket',
-        replace=True
-    )
+    filename = f"lastfm_api/{country}/{country}_{timestamp}.json"
+    
+    try:
+        hook = S3Hook(aws_conn_id='aws_conn')
+        hook.load_string(
+            string_data=json.dumps(data, indent=4),
+            key=filename,
+            bucket_name='bucket',
+            replace=True
+        )
+        logging.info(f"Данные загружены в S3 успешно.")
+    except Exception as er:
+        logging.error(f'Ошибка при загрузке данных в S3. {er}')
 
 
 default_args = {
@@ -56,16 +62,24 @@ default_args = {
 with DAG(
     default_args=default_args,
     dag_id='raw_from_api_to_s3',
-    description='Extract and Load raw data from API to S3',
+    description='Extract raw data from API to S3',
     tags=['raw', 's3', 'data lake', 'api'],
     start_date = days_ago(1),
     schedule_interval='0 9 * * *',
     catchup=False
 ) as dag:
     
+    start = EmptyOperator(
+        task_id='start'
+    )
+
     extract_data_to_s3 = PythonOperator(
         task_id='extract_data_to_s3',
         python_callable=extract_data_to_s3
     )
 
-    extract_data_to_s3
+    end = EmptyOperator(
+        task_id='end'
+    )
+
+    start >> extract_data_to_s3 >> end
